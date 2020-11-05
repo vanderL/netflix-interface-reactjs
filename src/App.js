d2b169c38d54ad80a9a399aeaa1843c05a11e829
vanderL <vnd.vander@gmail.com>
2020-11-04 22:47:12 -0300

    git init

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aqui será a interface principal da netflix!!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.netflix.com/browse"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          netflix.com/browse
         </a>
       </header>
     </div>
