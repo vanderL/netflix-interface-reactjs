ae9911246baf735a27baf7d39c0f510b8fea73d9
vanderL <vnd.vander@gmail.com>
2020-11-07 19:26:06 -0300

    Structure FuturedMovie - Manipulation API

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React, { useEffect, useState } from 'react'
 import css from './App.css'
 import Tmdb from './Tmdb'
 import MovieRow from './components/MovieRow/index'
+import FeaturedMovie from './components/FeaturedMovie/index'
 
 export default () => {
 
   const [moviesList, setMoviesList] = useState([])
+  const [featuredData, setFeaturedData] = useState([])
 
   useEffect(() =>{
     const loadAll = async () => {
       //Pegando a lista!
       let list = await Tmdb.getHomeList()
       setMoviesList(list)
+
+      //Pegando o Featured
+      let originals = list.filter(i => i.slug === 'originals')
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+      let chosen = originals[0].items.results[randomChosen]
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+      setFeaturedData(chosenInfo)
+
+      console.log(chosenInfo)
+      
     }
 
     loadAll()
@@ -20,6 +32,11 @@ export default () => {
 
   return (
     <div className="page">
+
+      {featuredData  &&
+        <FeaturedMovie item={featuredData} />
+      }
+
       <section className="lists">
         {moviesList.map((item, key) => (
           
