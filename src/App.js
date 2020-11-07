33165d143595c5ef2c6b8927812c35efe0384223
vanderL <vnd.vander@gmail.com>
2020-11-07 00:17:32 -0300

    MovieRow

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import css from './App.css'
 import Tmdb from './Tmdb'
+import MovieRow from './components/MovieRow/index'
 
 export default () => {
+
+  const [moviesList, setMoviesList] = useState([])
 
   useEffect(() =>{
     const loadAll = async () => {
       //Pegando a lista!
+      let list = await Tmdb.getHomeList()
+      setMoviesList(list)
     }
 
     loadAll()
   }, [])
 
-export default () => {
+
   return (
-    <div>
-      Olá Mundo!
+    <div className="page">
+      <section className="lists">
+        {moviesList.map((item, key) => (
+          
+          <MovieRow key={key} title={item.title} items={item.items} />
+
+        ))}
+      </section>
     </div>
   )
 }
